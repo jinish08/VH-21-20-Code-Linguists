85366d337baad8de894f9f27d927bc82c07d6733
Jinish Shah <jinishshah08@gmail.com>
2021-10-02 13:34:54 +0530

    add frontend

---
+++ web/frontend/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from "./Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ web/frontend/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>Team Linguists - 20</h1>
+      <h2>Members</h2>
+      <div>
+        <ul>
+          <li>Ojas Karmarker</li>
+          <li>Lokita</li>
+          <li>Sanika</li>
+          <li>Jinish Shah</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Home;
